22e67132e55e012a531af922fd732ac91df96ed9
Laur Joost <daremion@gmail.com>
2017-05-19 09:50:24 +0300

    Something blocks Moveit!

--- src/planning_tutorial/src/planning_tutorial_moveGroup.cpp
+++ src/planning_tutorial/src/planning_tutorial_moveGroup.cpp
@@ -4,11 +4,7 @@
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <ros/ros.h>
 
-
-
-
-
-
+const bool revizualise = true;
 
 int main(int argc, char **argv) {
 	ros::init(argc, argv, "moveGroup");
@@ -23,5 +19,29 @@ int main(int argc, char **argv) {
 
 	ROS_INFO("Ref frame: %s", group.getPlanningFrame().c_str());
 	ROS_INFO("Effector: %s", group.getEndEffectorLink().c_str());
+
+	geometry_msgs::Pose target_pose1;
+	target_pose1.orientation.w = 1.0;
+	target_pose1.position.x = 0.0;
+	target_pose1.position.y = -0.7;
+	target_pose1.position.z = 1.0;
+	group.setPoseTarget(target_pose1);
+
+	moveit::planning_interface::MoveGroup::Plan my_plan;
+	ROS_INFO("HERE!");
+	group.move();
+
+	//ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");
+	/* Sleep to give Rviz time to visualize the plan. */
+	//sleep(5.0);
+
+    ROS_INFO("Visualizing plan 1 (again)");
+    display_trajectory.trajectory_start = my_plan.start_state_;
+    display_trajectory.trajectory.push_back(my_plan.trajectory_);
+    display_publisher.publish(display_trajectory);
+    /* Sleep to give Rviz time to visualize the plan. */
+    //sleep(5.0);
+	ROS_INFO("Snooze!");
+
 	return 0;
 }
